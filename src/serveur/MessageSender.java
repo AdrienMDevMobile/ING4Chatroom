639a7c3ca7352c690b52fae3f7adc76412142358
AdrienpMichel <adrienpmichel@gmail.com>
2017-11-07 10:09:54 +0100

    Creation MessageSEnder pour serveur

---
+++ src/serveur/MessageSender.java
@@ -0,0 +1,18 @@
+package serveur;
+
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MessageSender {
+
+	private List<Socket> socketList;
+	
+	public MessageSender(){
+		socketList = new ArrayList<Socket>();
+	}
+	
+	
+	
+	
+}
