a70ee1928bb60bc7f2c5640c2d365883a806b07a
Lucas <lucas.heiligenstein@gmail.com>
2017-11-07 09:52:08 +0100

    Création de la classe ModelMessage Serializable

---
+++ src/main/ModelMessage.java
@@ -0,0 +1,46 @@
+package main;
+
+import java.io.Serializable;
+
+public class ModelMessage implements Serializable {
+	
+	private String pseudo;
+	private String message;
+	private String date;
+	
+	
+	public ModelMessage(String pseudo,String message, String date){
+		this.pseudo = pseudo;
+		this.message = message;
+		this.date = date;
+		
+	}
+	
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
